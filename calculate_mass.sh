1d8cbb661cc746086e75df6c309ff100b27963f0
Paul Boone <narcissus.pong@gmail.com>
2016-04-29 16:25:19 -0400

    sum the total mass from the masses of the individual gasses

--- calculate_mass.sh
+++ calculate_mass.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-output_dir = $1
-regexp="your regexp here"
+output_dir=$1
 
-grep $regexp $output_dir/*00.data | split_row.py
+line_regexp="Average loading absolute \[milligram\/gram framework\].*+\/-"
+value_regexp="[0-9\.]*"
+
+total_mass=0.0
+
+for gas_mass in $(
+  grep --only-matching "$line_regexp" $output_dir/*00.data |
+    grep --only-matching "$value_regexp" ); do
+  
+  total_mass=$( echo "$total_mass + $gas_mass" | bc -l )
+done
+
+echo $total_mass
